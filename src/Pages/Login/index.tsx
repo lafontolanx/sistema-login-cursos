38d2d169ad57c3e91dede2434c5bb53523305a1a
Laura Fontolan <laura_fontolan@hotmail.com>
2022-10-26 23:24:52 -0300

    inserting handle change

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,33 +1,40 @@
 import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
 import { Container, Form } from "./styles";
+import Botao from "../../Components/Botao";
+import Input from "../../Components/Input";
 
 export default function Login() {
+
+  const handleChange = (event: { target: { name: string; value: any; }; }) => {
+    console.log('Digitando...', event.target.name, event.target.value)
+  }
+
   return (
     <Container>
       <Form>
         <h1>Login</h1>
         
-        <div>
-            <TextField
-              id="outlined-helperText"
-              label="E-mail"
-              size="medium"
-              defaultValue=""
-            />
-        </div>
+        <Input
+          name='email'
+          placeholder='Digite o seu e-mail'
+          onChange={handleChange}
+          type='email'
+        />
 
-        <div>
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              size="medium"
-            />
-        </div>
+        <Input
+          name='password'
+          placeholder='Digite a sua senha'
+          onChange={handleChange}
+          type='password'
+        />
 
-        <Button variant="contained" size="large">login</Button>
+        <Botao
+          type="submit"
+          text="Entrar"
+          onClick={() => {}}
+          disabled={false}
+        />
+
       </Form>
     </Container>
   );
