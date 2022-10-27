0b56dca09052b8271e3749c3fd9c571b87673c01
Laura Fontolan <laura_fontolan@hotmail.com>
2022-10-26 23:23:25 -0300

    configuring routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRoutes from './routes';
-
+import AppRoutes from './Routes/routes';
 import GlobalStyle from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
